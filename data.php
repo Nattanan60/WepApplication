0e48ce1451b0e893b937202a2b77f01609685b9d
Nattanan60 <taekup_love@hotmail.co.th>
2020-02-20 16:51:11 +0700

    commit3

---
+++ data.php
@@ -0,0 +1,39 @@
+<?php
+//teamname
+
+$team_name = $_GET['team_name'];
+//name
+
+$name_mem1 = $_GET['name_mem1'];
+$name_mem2 = $_GET['name_mem2'];
+$name_mem3 = $_GET['name_mem3'];
+$name_mem4 = $_GET['name_mem4'];
+$name_mem5 = $_GET['name_mem5'];
+$name_mem6 = $_GET['name_mem6'];
+//ingame name
+$nameingame_mem1 = $_GET['nameingame_mem1'];
+$nameingame_mem2 = $_GET['nameingame_mem2'];
+$nameingame_mem3 = $_GET['nameingame_mem3'];
+$nameingame_mem4 = $_GET['nameingame_mem4'];
+$nameingame_mem5 = $_GET['nameingame_mem5'];
+$nameingame_mem6 = $_GET['nameingame_mem6'];
+
+echo $team_name;
+echo $name_mem1;
+echo $name_mem2;
+echo $name_mem3;
+echo $name_mem4;
+echo $name_mem5;
+echo $name_mem6;
+
+echo $nameingame_mem1;
+echo $nameingame_mem2;
+echo $nameingame_mem3;
+echo $nameingame_mem4;
+echo $nameingame_mem5;
+echo $nameingame_mem6;
+
+
+
+
+?>
