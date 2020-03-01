97e8c01c5d67f9bc5add618fc4723128ae29286d
Nattanan60 <taekup_love@hotmail.co.th>
2020-03-01 16:28:30 +0700

    'commit4'

---
+++ main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="cssall.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PUBG Phayao Series LEAGUE</title>
+    <style>
+        body {
+          background-image: url('1.jpg');
+        }
+    </style>
+</head>
+<body>
+  
+    <center>
+        <form action="page1.html" method="post">
+                <input type="submit" value="สมัครเข้าการแข่งขัน" class="button1">
+        </form>
+        <form action="data.php" method="post">
+                <input type="submit" value="ตรวจสอบข้อมูลทีม" class="button2">
+        </form>
+    </center>
+</body>
+</html>
